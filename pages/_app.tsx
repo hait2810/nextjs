23dd3da552869585a7cefae12918492ad58ecc43
Utinot <levanthuan412002@gmail.com>
2022-08-04 10:36:50 +0700

    bach_end

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,10 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/layout'
 import { AppPropsWithLayout } from '../models/layout';
+import signin from '../components/lognin/signin';
 
-
-import './../components/Header/header.css';
-import './../components/Header/search';
+// import './../components/Header/header.css';
+// import './../components/Header/search';
 // import 'https://unpkg.com/aos@next/dist/aos.js';
 
 
