7455e694b76fa870f6f009ee501ce7f872bd93da
Hai <checked.kenvip10@gmail.com>
2022-08-04 11:42:37 +0700

    BE: feature Add Product

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -7,6 +7,15 @@ const getProducts = async (req, res) => {
     res.status(400).json({ message: "Không hiển thị được" });
   }
 };
+const addProduct = async (req, res) => {
+    try {
+      const product = await Products(req.body).save();
+      res.json(product);
+    } catch (error) {
+      res.status(400).json({ message: "Không thêm được" });
+    }
+  };
 module.exports = {
-  getProducts
+  getProducts,
+  addProduct
 };
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const {
-  getProducts
+  getProducts, addProduct
 } = require("../controllers/product");
 const routerProduct = express.Router();
 
+
 routerProduct.get("/products", getProducts);
+routerProduct.post("/products", addProduct)
+
 module.exports = routerProduct;
