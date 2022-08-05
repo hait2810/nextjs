b8ceecb1dd3b07526aa2414dfd65dee2090982a9
Utinot <levanthuan412002@gmail.com>
2022-08-05 09:36:57 +0700

    up user

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const routerProduct = require("./routes/products");
-const routerCategory = require("./routes/categorys");
+// const routerProduct = require("./routes/products");
+// const routerCategory = require("./routes/categorys");
 const routerUser = require("./routes/user");
 const url =
   "mongodb+srv://haidev:ahai2001@cluster0.ziydc.mongodb.net/?retryWrites=true&w=majority";
@@ -19,8 +19,8 @@ connect();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(routerProduct);
-app.use(routerCategory);
+// app.use(routerProduct);
+// app.use(routerCategory);
 app.use(routerUser)
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
