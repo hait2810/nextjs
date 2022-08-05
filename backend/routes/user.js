53e572fe29ab8f6208fc36af36e141e9e6c4ba03
Utinot <levanthuan412002@gmail.com>
2022-08-05 17:33:43 +0700

    user

---
+++ backend/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { signup, signin, removeUser, updateUser, getUser, getUsers } = require('../controllers/user')
+const routerUser = express.Router() 
+
+routerUser.post("/signup", signup)
+routerUser.post("/signin", signin)
+routerUser.delete('/user/:id', removeUser)
+routerUser.put('/user/:id', updateUser)
+routerUser.get('/users/', getUsers)
+module.exports = routerUser
