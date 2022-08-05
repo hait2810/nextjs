329120e0757897c5f2242f8e8fd37e89ce7619a4
quanth <quanthph14921@fpt.edu.vn>
2022-08-05 10:51:35 +0700

    feature: Add Category + Delete

--- api/categorys.tsx
+++ api/categorys.tsx
@@ -7,3 +7,21 @@ export const getCategorys = () => {
         const url = `categorys`;
         return instance.get(url)
 }
+export const AddCategory = (category : Icategory) =>{
+        const url = `categorys`;
+        return instance.post(url, category)
+}
+
+export const UpdateCategory = (category: Icategory, id : any) =>{
+        const url = `categorys/${id}`;
+        return instance.put(url,category)
+}
+export const DeleteCategory = (id : any) =>{
+        const url = `categorys/${id}`;
+        return instance.delete(url)
+}
+
+export const getCategoryId = (id : any) =>{
+        const url = `categorys/${id}`;
+        return instance.get(url)
+}
