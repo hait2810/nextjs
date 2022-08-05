958e2533164a64ff24a819c6500e17b4ce58e1b5
anhntph14690 <anhntph14690@fpt.edu.vn>
2022-08-05 17:03:35 +0700

    feature: list product

--- pages/admin/categorys/index.tsx
+++ pages/admin/categorys/index.tsx
@@ -48,7 +48,7 @@ const ListCategory = (props: Props) => {
       </tr>
     </thead>
     <tbody>
-    {categorys?.map((item : any , index : number) =>{
+    {categorys.data?.map((item : any , index : number) =>{
         return <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
         <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
         {index + 1}
--- pages/admin/products/add/index.tsx
+++ pages/admin/products/add/index.tsx
@@ -87,7 +87,7 @@ const AddProduct = (props: Props) => {
                                             <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Chọn danh mục</label>
 <select {...register('category', {required:true})} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 <option selected>Choose a country</option>
-    {categorys && categorys?.map((item:any) => {       
+    {categorys && categorys.data?.map((item:any) => {       
         return  <option key={item._id} value={item._id}>{item.name}</option>       
     })}
 </select>
